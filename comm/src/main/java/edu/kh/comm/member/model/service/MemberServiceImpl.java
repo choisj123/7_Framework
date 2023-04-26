f210118560e04e221aa3230898c213727a7b20fd
CHOISUJIN <98chltnwls@gmail.com>
2023-04-26 09:56:28 +0900

    암호화

--- comm/src/main/java/edu/kh/comm/member/model/service/MemberServiceImpl.java
+++ comm/src/main/java/edu/kh/comm/member/model/service/MemberServiceImpl.java
@@ -1,6 +1,9 @@
 package edu.kh.comm.member.model.service;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import edu.kh.comm.member.model.dao.MemberDAO;
@@ -21,10 +24,41 @@ public class MemberServiceImpl implements MemberService {
 	 * 
 	 */
 	
+	// 암호화를 위한 bcrypt 객체 의존성 주입(DI)
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
+
+	private Logger logger = LoggerFactory.getLogger(MemberServiceImpl.class);
+	
+	
 	/** 로그인 서비스 구현
 	 */
 	@Override
 	public Member login(Member inputMember) {
+		
+		// 전달 받은 비밀번호를 암호화하여
+		// DB에서 조회환 비밀번호와 비교 (DB에서 비교 X)
+		
+		// sha 방식 암호화
+		// A회원 / 비밀번호 1234 -> 암호화 : abcd
+		// B회원 / 비밀번호 1234 -> 암호화 : abcd (암호화 시 변경된 내용이 같음)
+		
+		// Bcrypt 암호화 : 암호화 하기 전에 salt를 추가하여 변형된 상태로 암호화를 진행
+		// A회원 / 비밀번호 1234 -> 암호화 : abcd
+		// B회원 / 비밀번호 1234 -> 암호화 : @bdg
+		
+		// * 매번 암호화되는 비밀번호가 달라져서 DB에서 직접 비교 불가능
+		// 대신 Bcrypt 암호화를 지원하는 객체가 
+		// 이를 비교하는 기능(메소드) 가지고 있어서 이를 활용하면 된다!
+		
+		// ** Bcrypt 암호화를 사용하기 위해 이를 지원하는 Spring-security 모듈 추가 **
+		logger.debug("암호화 전 : " + inputMember.getMemberPw() + " / 암호화 후 : " + bcrypt.encode(inputMember.getMemberPw()));
+		logger.debug("암호화 전 : " + inputMember.getMemberPw() + " / 암호화 후 : " + bcrypt.encode(inputMember.getMemberPw()));
+		logger.debug("암호화 전 : " + inputMember.getMemberPw() + " / 암호화 후 : " + bcrypt.encode(inputMember.getMemberPw()));
+		logger.debug("암호화 전 : " + inputMember.getMemberPw() + " / 암호화 후 : " + bcrypt.encode(inputMember.getMemberPw()));
+		logger.debug("암호화 전 : " + inputMember.getMemberPw() + " / 암호화 후 : " + bcrypt.encode(inputMember.getMemberPw()));
+		
+		
 		
 		Member loginMember = dao.login(inputMember);
 		
